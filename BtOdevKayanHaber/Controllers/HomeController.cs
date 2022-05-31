b98ca74be770d51d338f29218c0202ce6050e1aa
Emre Çakıral <emrecakiral@outlook.com>
2022-05-31 19:11:45 +0300

    MVC Kayan haber

--- BtOdevKayanHaber/Controllers/HomeController.cs
+++ BtOdevKayanHaber/Controllers/HomeController.cs
@@ -8,11 +8,11 @@ namespace BtOdevKayanHaber.Controllers
         public IActionResult Index()
         {
             List<Haber> haberList = new List<Haber>();
-            haberList.Add(new Haber { baslik = "Haber 1", yazi = "C# Programlama dilini öğrendik... Artık c# dilini biliyoruz.", tarih = Convert.ToDateTime(DateTime.Now.ToString("dd MM yy")) });
-            haberList.Add(new Haber { baslik = "Haber 2", yazi = "Sql Server Öğrendik. Hepimiz birer DBA'yiz..", tarih = Convert.ToDateTime(DateTime.Now.ToString("dd MM yy")) });
-            haberList.Add(new Haber { baslik = "Haber 3", yazi = "EF Core Örendik. Artık veritabanı ile sqli bağlıyoruz.", tarih = Convert.ToDateTime(DateTime.Now.ToString("dd MM yy")) });
-            haberList.Add(new Haber { baslik = "Haber 4", yazi = "Ve nihayet webteyiz. Hatta Asp.Net Core öğreneceğiz.", tarih = Convert.ToDateTime(DateTime.Now.ToString("dd MM yy")) });
-            haberList.Add(new Haber { baslik = "Haber 5", yazi = "Hoca dapperda anlatacakmış.", tarih = Convert.ToDateTime(DateTime.Now.ToString("dd MM yy")) });
+            haberList.Add(new Haber { baslik = "Haber 1", yazi = "C# Programlama dilini öğrendik... Artık c# dilini biliyoruz.", tarih = DateTime.Now });
+            haberList.Add(new Haber { baslik = "Haber 2", yazi = "Sql Server Öğrendik. Hepimiz birer DBA'yiz..", tarih = DateTime.Now });
+            haberList.Add(new Haber { baslik = "Haber 3", yazi = "EF Core Örendik. Artık veritabanı ile sqli bağlıyoruz.", tarih = DateTime.Now });
+            haberList.Add(new Haber { baslik = "Haber 4", yazi = "Ve nihayet webteyiz. Hatta Asp.Net Core öğreneceğiz.", tarih = DateTime.Now });
+            haberList.Add(new Haber { baslik = "Haber 5", yazi = "Hoca dapperda anlatacakmış.", tarih = DateTime.Now });
             return View(haberList);
         }
     }
